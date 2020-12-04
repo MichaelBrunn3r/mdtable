f499e2a67baa758616a327f6570105032f82a622
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-12-04 19:55:42 +0100

    Added TableRow

--- src/routes/mdtable/InteractiveTable.svelte
+++ src/routes/mdtable/InteractiveTable.svelte
@@ -1,7 +1,8 @@
 <script>
-	export let headers = ["Tables", "Are", "Cool"];
-	export let rows = [["col 1 is", "left aligned", "$1600"],["col 2 is","centered", "$12"],["col 3 is", "right-aligned", "$1"],["col 1 is", "left aligned", "$1600"],["col 1 is", "left aligned", "$1600"]];
+	import TableRow from './TableRow.svelte';
 
+	export let header = ["Tables", "Are", "Cool"];
+	export let rows = [["col 1 is", "left aligned", "$1600"],["col 2 is","centered", "$12"],["col 3 is", "right-aligned", "$1"],["col 1 is", "left aligned", "$1600"],["col 1 is", "left aligned", "$1600"]];
 </script>
 
 <style>
@@ -14,15 +15,6 @@
 
 	tbody tr:nth-child(even) {
 		background-color: #f6f8fa;
-	}
-
-	th {
-		font-weight: 600;
-	}
-
-	th,td {
-		border: 1px solid #dfe2e5;
-		padding: 6px 13px;
 	}
 
 	button {
@@ -56,17 +48,13 @@
 		<table class="table">
 			<thead>
 				<tr>
-					{#each headers as header}
-						<th>{header}</th>
-					{/each}
+					<TableRow isHeader={true} entries={header}/>
 				</tr>
 			</thead>
 			<tbody>
 				{#each rows as row}
 					<tr>
-						{#each row as entry}
-							<td>{entry}</td>
-						{/each}
+						<TableRow entries={row}/>
 					</tr>
 				{/each}
 			</tbody>
---
+++ src/routes/mdtable/TableRow.svelte
@@ -0,0 +1,24 @@
+<script>
+	export let isHeader = false;
+	export let entries;
+</script>
+
+<style>
+	th {
+		font-weight: 600;
+	}
+
+	td, th {
+		border: 1px solid #dfe2e5;
+		padding: 6px 13px;
+	}
+</style>
+
+
+{#each entries as entry}
+	{#if isHeader}
+		<th>{entry}</th>
+	{:else}
+		<td>{entry}</td>
+	{/if}
+{/each}
