f3970465b5fc9d4fd8d7e54cc00d4c2e9ad1de08
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-12-04 09:48:56 +0100

    Setup gh-pages deployment

--- scripts/gh-pages.js
+++ scripts/gh-pages.js
@@ -1,10 +1,10 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish(
-    '__sapper__/export/<REPONAME>',
+    '__sapper__/export',
     {
-        branch: 'gh-pages',
-        repo: 'https://github.com/MichaelBrunn3r/<REPONAME>.git',
+        branch: 'main',
+        repo: 'https://github.com/MichaelBrunn3r/MichaelBrunn3r.github.io.git',
         user: {
             name: 'MichaelBrunn3r',
             email: 'MichaelBrunn3r@gmail.com'
