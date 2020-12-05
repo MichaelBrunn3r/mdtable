b48a7ebd55463c2d09decb902e1eb0bd87abe7c8
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-12-05 14:26:17 +0100

    Added basic navbar

---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,18 @@
+<style>
+	.avatar {
+		width: 30px;
+		height: 30px;
+	}
+</style>
+
+<nav class="navbar navbar-dark bg-dark justify-content-between">
+	<a class="navbar-brand" href="/">
+		<img class="avatar rounded-circle" src="https://avatars2.githubusercontent.com/u/19626539?s=80" alt="Avatar" loading="lazy">
+		mdtable
+	</a>
+	<div>
+		<button type="button" class="btn" aria-label="Copy to Clipboard">
+			<i aria-hidden="true" class="fas fa-copy text-light"></i>
+		</button>
+	</div>
+</nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
 	import EditableTable from './EditableTable.svelte';
+	import Navbar from './Navbar.svelte';
 </script>
+
+<Navbar />
 
 <EditableTable />
