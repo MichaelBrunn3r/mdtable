cd6edb569bf8b18199f7aa21855d31fa3575b0e9
Michael Brunner <MichaelBrunn3r@gmail.com>
2020-12-08 13:57:49 +0100

    Renamed CellPosition to Position

--- src/routes/selection.ts
+++ src/routes/selection.ts
@@ -1,30 +1,30 @@
 import {constrain} from './utils';
 
-export class CellPosition {
-	rowIdx: number;
-	columnIdx: number;
+export class Position {
+	x: number;
+	y: number;
 
-	constructor(rowIdx: number, columnIdx: number) {
-		this.rowIdx = rowIdx;
-		this.columnIdx = columnIdx;
+	constructor(x: number, y: number) {
+		this.x = x;
+		this.y = y;
 	}
 }
 
 export class Selection {
-	startPos: CellPosition;
-	endPos: CellPosition;
+	startPos: Position;
+	endPos: Position;
 
 	constructor(startRow: number, startColumn: number, endRow: number, endColumn: number) {
-		this.startPos = new CellPosition(startRow, startColumn);
-		this.endPos = new CellPosition(endRow, endColumn);
+		this.startPos = new Position(startColumn, startRow);
+		this.endPos = new Position(endColumn, endRow);
 	}
 
 	/**
 	 * Collapses the selection into the starting position
 	 */
 	collapse() {
-		this.endPos.rowIdx = this.startPos.rowIdx;
-		this.endPos.columnIdx = this.startPos.columnIdx;
+		this.endPos.x = this.startPos.x;
+		this.endPos.y = this.startPos.y;
 		return this;
 	}
 
@@ -32,10 +32,10 @@ export class Selection {
 	 * Translates the selection by an amount of rows and columns
 	 */
 	translate(rows: number, columns: number) {
-		this.startPos.rowIdx += rows;
-		this.startPos.columnIdx += columns;
-		this.endPos.rowIdx += rows;
-		this.endPos.columnIdx += columns;
+		this.startPos.x += columns;
+		this.startPos.y += rows;
+		this.endPos.x += columns;
+		this.endPos.y += rows;
 		return this;
 	}
 
@@ -43,15 +43,15 @@ export class Selection {
 	 * Constrain the selection to be inside an area
 	 */
 	constrain(minRow: number, minColumn: number, maxRow: number, maxColumn: number) {
-		this.startPos.rowIdx = constrain(this.startPos.rowIdx, minRow, maxRow);
-		this.startPos.columnIdx = constrain(this.startPos.columnIdx, minColumn, maxColumn);
-		this.endPos.rowIdx = constrain(this.endPos.rowIdx, minRow, maxRow);
-		this.endPos.columnIdx = constrain(this.endPos.columnIdx, minColumn, maxColumn);
+		this.startPos.x = constrain(this.startPos.x, minColumn, maxColumn);
+		this.startPos.y = constrain(this.startPos.y, minRow, maxRow);
+		this.endPos.x = constrain(this.endPos.x, minColumn, maxColumn);
+		this.endPos.y = constrain(this.endPos.y, minRow, maxRow);
 		return this;
 	}
 
 	setEndPos(rowIdx: number, columnIdx: number) {
-		this.endPos = new CellPosition(rowIdx, columnIdx);
+		this.endPos = new Position(rowIdx, columnIdx);
 		return this;
 	}
 
@@ -59,10 +59,10 @@ export class Selection {
 	 * Predicate wether a cell is inside the selection
 	 */
 	contains(rowIdx: number, columnIdx: number) {
-		if(rowIdx < this.startPos.rowIdx && rowIdx < this.endPos.rowIdx) return false;
-		if(rowIdx > this.startPos.rowIdx && rowIdx > this.endPos.rowIdx) return false;
-		if(columnIdx > this.startPos.columnIdx && columnIdx > this.endPos.columnIdx) return false;
-		if(columnIdx < this.startPos.columnIdx && columnIdx < this.endPos.columnIdx) return false;
+		if(columnIdx > this.startPos.x && columnIdx > this.endPos.x) return false;
+		if(columnIdx < this.startPos.x && columnIdx < this.endPos.x) return false;
+		if(rowIdx < this.startPos.y && rowIdx < this.endPos.y) return false;
+		if(rowIdx > this.startPos.y && rowIdx > this.endPos.y) return false;
 
 		return true;
 	}
@@ -71,27 +71,27 @@ export class Selection {
 	 * Predicate wether a cell is at the top edge of the selection
 	 */
 	atTopEdge(rowIdx: number, columnIdx: number) {
-		return this.contains(rowIdx, columnIdx) && rowIdx === Math.min(this.startPos.rowIdx, this.endPos.rowIdx);
+		return this.contains(rowIdx, columnIdx) && rowIdx === Math.min(this.startPos.y, this.endPos.y);
 	}
 
 	/**
 	 * Predicate wether a cell is at the bottom edge of the selection
 	 */
 	atBottomEdge(rowIdx: number, columnIdx: number) {
-		return this.contains(rowIdx, columnIdx) && rowIdx === Math.max(this.startPos.rowIdx, this.endPos.rowIdx);
+		return this.contains(rowIdx, columnIdx) && rowIdx === Math.max(this.startPos.y, this.endPos.y);
 	}
 
 	/**
 	 * Predicate wether a cell is at the left edge of the selection
 	 */
 	atLeftEdge(rowIdx: number, columnIdx: number) {
-		return this.contains(rowIdx, columnIdx) && columnIdx === Math.min(this.startPos.columnIdx, this.endPos.columnIdx);
+		return this.contains(rowIdx, columnIdx) && columnIdx === Math.min(this.startPos.x, this.endPos.x);
 	}
 
 	/**
 	 * Predicate wether a cell is at the right edge of the selection
 	 */
 	atRightEdge(rowIdx: number, columnIdx: number) {
-		return this.contains(rowIdx, columnIdx) && columnIdx === Math.max(this.startPos.columnIdx, this.endPos.columnIdx);
+		return this.contains(rowIdx, columnIdx) && columnIdx === Math.max(this.startPos.x, this.endPos.x);
 	}
 }
